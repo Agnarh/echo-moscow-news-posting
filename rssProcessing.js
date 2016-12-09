5396498fe711b22b96c0a5911c637116a761a172
Iurii Perederii <iurii_perederii@epam.com>
2016-12-09 19:10:04 +0400

    implemented rss.xml file processing - creating and sending

--- rssProcessing.js
+++ rssProcessing.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var Client = require('ftp');
+var iconv = require('iconv-lite');
 var config = require('./config');
 var rssSettings = config.rssSettings;
 var ftpSettings = config.ftpSettings;
@@ -7,5 +10,41 @@ var _ = require('lodash');
 var templateFunc = _.template(rssTemplate);
 
 module.exports = {
-    generateXMLString: news => templateFunc({ news })
+    generateXMLString: news => templateFunc({ news }),
+    writeRssFile: function (xmlString) {
+        return new Promise(function (fulfill, reject) {
+            var buffer = iconv.encode(xmlString, 'win1251');
+            
+            fs.writeFile(rssSettings.rssFileName, buffer, function (error) {
+                if (error) {
+                    reject(error.message);
+                }
+
+                fulfill();
+            });
+        });
+    },
+    sendRSSFile: function () {
+        return new Promise(function (fulfill, reject) {
+            var client = new Client();
+            
+            client.on('ready', function () {
+                client.put(rssSettings.rssFileName, rssSettings.rssDestFilePath, function (error) {
+                    if (error) {
+                        client.destroy();
+                        reject(error.message);
+                    }
+                    
+                    client.end();
+                    fulfill();
+                });
+            });
+            
+            client.connect({
+                host: ftpSettings.serverIP,
+                user: ftpSettings.login,
+                password: ftpSettings.password
+            });
+        });
+    }
 }
