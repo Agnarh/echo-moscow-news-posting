502fff7e05bd7e742ce3e839ef074df147902f38
Iurii Perederii <iurii_perederii@epam.com>
2016-12-09 14:27:55 +0400

    added initial rss processing

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var newsProcessing = require('./newsProcessing');
+var rssProcessing = require('./rssProcessing');
 
 new Promise(newsProcessing.getLatestNewsNumber)
-.then(newsProcessing.getNewsUrls)
-.then(urls => Promise.all(urls.map(newsProcessing.processNewsUrl)))
-.then(newsProcessing.processResultNews)
-.catch(function (error) {
-    console.log(error);
-});
+    .then(newsProcessing.getNewsUrls)
+    .then(urls => Promise.all(urls.map(newsProcessing.processNewsUrl)))
+    .then(newsProcessing.processResultNews)
+    .then(rssProcessing.generateXMLString)
+    .then(function (xml) {
+        // console.log(xml);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
